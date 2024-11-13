3ad966f26ac8115975917b3355013eacf30f283e
ObiOneKenobii <jimijnrrruji@gmail.com>
2024-11-13 16:47:42 +0100

    Corrections To FlightTicketService

--- HotelFlightMVC/Services/FlightTicketService.cs
+++ HotelFlightMVC/Services/FlightTicketService.cs
@@ -19,25 +19,10 @@ namespace HotelFlightMVC.Services
             _logger = logger;
         }
 
-        //public async Task<IEnumerable<FlightTicket>> GetAllFlightTicketsAsync()
-        //{
-        //    try
-        //    {
-        //        return await _httpClient.GetFromJsonAsync<IEnumerable<FlightTicket>>("https://hotelflightapi.onrender.com");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        _logger.LogError(ex, "An error occurred while fetching flight tickets.");
-        //        throw;
-        //    }
-        //}
-
-
         public async Task<IEnumerable<FlightTicket>> GetAllFlightTicketsAsync()
         {
             try
             {
-                // Assuming the endpoint is at "/api/FlightTicket"
                 return await _httpClient.GetFromJsonAsync<IEnumerable<FlightTicket>>("api/FlightTicket");
             }
             catch (Exception ex)
@@ -47,12 +32,11 @@ namespace HotelFlightMVC.Services
             }
         }
 
-
         public async Task<FlightTicket> GetFlightTicketAsync(int id)
         {
             try
             {
-                return await _httpClient.GetFromJsonAsync<FlightTicket>($"https://hotelflightapi.onrender.com/FlightTicket/{id}");
+                return await _httpClient.GetFromJsonAsync<FlightTicket>($"api/FlightTicket/{id}");
             }
             catch (Exception ex)
             {
@@ -65,7 +49,7 @@ namespace HotelFlightMVC.Services
         {
             try
             {
-                var response = await _httpClient.PostAsJsonAsync("https://hotelflightapi.onrender.com/api/FlightTicket", flightTicket);
+                var response = await _httpClient.PostAsJsonAsync("api/FlightTicket", flightTicket);
                 response.EnsureSuccessStatusCode();
                 return await response.Content.ReadFromJsonAsync<FlightTicket>();
             }
@@ -80,7 +64,7 @@ namespace HotelFlightMVC.Services
         {
             try
             {
-                var response = await _httpClient.PutAsJsonAsync($"https://hotelflightapi.onrender.com/api/FlightTicket/{flightTicket.Id}", flightTicket);
+                var response = await _httpClient.PutAsJsonAsync($"api/FlightTicket/{flightTicket.Id}", flightTicket);
                 response.EnsureSuccessStatusCode();
             }
             catch (Exception ex)
@@ -94,7 +78,7 @@ namespace HotelFlightMVC.Services
         {
             try
             {
-                var response = await _httpClient.DeleteAsync($"https://hotelflightapi.onrender.com/api/FlightTicket/{id}");
+                var response = await _httpClient.DeleteAsync($"api/FlightTicket/{id}");
                 response.EnsureSuccessStatusCode();
             }
             catch (Exception ex)
@@ -105,91 +89,3 @@ namespace HotelFlightMVC.Services
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//using HotelFlightMVC.Models;
-//using Newtonsoft.Json;
-//using System.Net.Http;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Collections.Generic;
-
-//namespace HotelFlightMVC.Services
-//{
-//    public class FlightTicketService
-//    {
-//        private readonly HttpClient _httpClient;
-//        private readonly CartService _cartService;
-
-//        public FlightTicketService(HttpClient httpClient, CartService cartService)
-//        {
-//            _httpClient = httpClient;
-//            _cartService = cartService;
-//        }
-
-//        public async Task<IEnumerable<FlightTicket>> GetAllFlightTickets()
-//        {
-//            var response = await _httpClient.GetAsync("api/FlightTicket");
-//            if (response.IsSuccessStatusCode)
-//            {
-//                var jsonData = await response.Content.ReadAsStringAsync();
-//                return JsonConvert.DeserializeObject<IEnumerable<FlightTicket>>(jsonData);
-//            }
-//            return null;
-//        }
-
-//        public async Task<FlightTicket> GetFlightTicket(int id)
-//        {
-//            var response = await _httpClient.GetAsync($"api/FlightTicket/{id}");
-//            if (response.IsSuccessStatusCode)
-//            {
-//                var jsonData = await response.Content.ReadAsStringAsync();
-//                return JsonConvert.DeserializeObject<FlightTicket>(jsonData);
-//            }
-//            return null;
-//        }
-
-//        public async Task<bool> CreateFlightTicket(FlightTicket flightTicket)
-//        {
-//            var jsonData = JsonConvert.SerializeObject(flightTicket);
-//            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-//            var response = await _httpClient.PostAsync("api/FlightTicket", content);
-//            return response.IsSuccessStatusCode;
-//        }
-
-//        public async Task<bool> UpdateFlightTicket(FlightTicket flightTicket)
-//        {
-//            var jsonData = JsonConvert.SerializeObject(flightTicket);
-//            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-//            var response = await _httpClient.PutAsync($"api/FlightTicket/{flightTicket.Id}", content);
-//            return response.IsSuccessStatusCode;
-//        }
-
-//        public async Task<bool> DeleteFlightTicket(int id)
-//        {
-//            var response = await _httpClient.DeleteAsync($"api/FlightTicket/{id}");
-//            return response.IsSuccessStatusCode;
-//        }
-
-//        // Method to add a flight ticket to the user's cart
-//        public async Task<bool> AddTicketToCart(string userId, FlightTicket flightTicket)
-//        {
-//            // Use the CartService to add the ticket to the cart
-//            return await _cartService.AddTicketToCart(userId, flightTicket);
-//        }
-//    }
-//}
